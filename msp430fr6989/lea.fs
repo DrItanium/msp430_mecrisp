a7cc812722b35f2f3b712a2faed941fe04243975
Joshua Scoggins <Theoretically.x64@gmail.com>
2018-12-26 14:22:08 -0800

    Initial file for the LEA

---
+++ msp430fr6989/lea.fs
@@ -0,0 +1,8 @@
+\ LEA - Low Energy Accelerator
+\ Module that is an execution unit for vector-based signal
+\ processing operations.
+\ Taken from the manual:
+\ The LEA is a 32-bit hardware device designed for operations
+\ that involve vector-based signal processing, such as 
+\ FIR, IIR, and FFT without CPU intervention. The LEA supports multiple
+\ commands, which are issued by CPU.
