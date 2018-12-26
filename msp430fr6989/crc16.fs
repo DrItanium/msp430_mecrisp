a7c4f279b6b1078c41f52817b40e3231f2b91119
Joshua Scoggins <Theoretically.x64@gmail.com>
2018-12-26 14:03:59 -0800

    Added simple crc16 words

--- msp430fr6989/crc16.fs
+++ msp430fr6989/crc16.fs
@@ -8,11 +8,33 @@ compiletoram
   CRC16_BASE + constant 
   compiletoram ;
 
-$00 defcrc16reg CRC16_DI // CRC DATA INPUT
-$02 defcrc16reg CRCDIRB // CRC data input reverse byte
-$04 defcrc16reg CRCINIRES // CRC initialization and result
-$06 defcrc16reg CRCINIRES // CRC result reverse byte
+$00 defcrc16reg CRC16DI // CRC DATA INPUT
+$02 defcrc16reg CRC16DIRB // CRC data input reverse byte
+$04 defcrc16reg CRC16INIRES // CRC initialization and result
+$06 defcrc16reg CRC16RESR // CRC result reverse byte
 compiletoflash
+\ Taken from the documentation for the module
+\ The CRC generator is first initialized by writing a 16-bit word 
+\ (seed) to the CRC Initialization and Result (CRCINIRES) register.
+\ Any data that should be included into the CRC calculation must
+\ be written to the CRC Data Input (CRCDI or CRCDIRB) register in
+\ the same order that the ordiginal CRC signature was calculated.
+\ The actual signature can be read from CRCINIRES register to compare
+\ the computed checksum with the expected checksum.
+\ 
+\ Signature generation describes a method of how the result of a 
+\ signature operation can be calculated. The calcuated signature,
+\ which is computed by an external tool, is called checksum in
+\ the following text. The checksum is stored in the product's memory
+\ and is used to check the correctness of the CRC operation result.
+
+\ simple words to access the crc16 engine
+: crc16:seed ( seed -- ) CRC16INIRES ! ;
+: crc16! ( value -- ) CRC16DI ! ;
+: crc16!r ( value -- ) CRC16DIRB ! ;
+: crc16@ ( -- value ) CRC16INIRES @ ;
+: crc16@r ( -- value ) CRC16RESR @ ;
+
 
 
 compiletoram
