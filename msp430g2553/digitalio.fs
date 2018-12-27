55e0053ed4fd7f8d1c7c9587058fae19e02d4555
Joshua Scoggins <Theoretically.x64@gmail.com>
2018-12-26 20:23:29 -0800

    Define the digitalio pins

---
+++ msp430g2553/digitalio.fs
@@ -0,0 +1,23 @@
+\ digital io pins for the msp430g2553
+compiletoflash
+\ 8-bit addresses
+$20 constant P1IN
+$21 constant P1OUT
+$22 constant P1DIR
+$23 constant P1IFG
+$24 constant P1IES
+$25 constant P1IE
+$26 constant P1SEL
+$41 constant P1SEL2
+$27 constant P1REN
+$28 constant P2IN
+$29 constant P2OUT
+$2A constant P2DIR
+$2B constant P2IFG
+$2C constant P2IES
+$2D constant P2IE
+$2E constant P2SEL
+$42 constant P2SEL2
+$2F constant P2REN
+
+compiletoram
