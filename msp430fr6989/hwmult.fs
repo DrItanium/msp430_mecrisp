a02881a006ac9d3f8e73e8ca48c448707bdb4331
Joshua Scoggins <Theoretically.x64@gmail.com>
2018-12-26 19:06:04 -0800

    Greatly simplify access to the multiplication operations

--- msp430fr6989/hwmult.fs
+++ msp430fr6989/hwmult.fs
@@ -31,37 +31,16 @@ $2A defhwmulreg HWMULT_RES3 \ 32 x 32 result 3 - most significant word
 $2C defhwmulreg HWMULT_MPY32CTL0 \ MPY32 control 0
 
 compiletoflash 
-: hwmult:16x16accum-clear ( -- ) 
-  0 HWMULT_RESLO !
-  0 HWMULT_RESHI ! ;
-: hwmult:32x32accum-clear ( -- ) 
-  0 HWMULT_RES0 ! 
-  0 HWMULT_RES1 ! 
-  0 HWMULT_RES2 ! 
-  0 HWMULT_RES3 ! ;
-: hwmult:16x16accum ( -- d ) 
-  HWMULT_RESLO @
-  HWMULT_RESHI @ ;
-: hwmult:32x32accum-low-dword ( -- d )
-  HWMULT_RES0 @
-  HWMULT_RES1 @ ;
-: hwmult:32x32accum-high-dword ( -- d )
-  HWMULT_RES2 @
-  HWMULT_RES3 @ ;
-: hwmult:32x32accum ( -- dlo dhi )
-  hwmult:32x32accum-low-dword
-  hwmult:32x32accum-high-dword ;
-: hwmult:16x16sum-ext ( -- se ) HWMULT_SUMEXT @ ;
 : hwmult:def_16bit_operation ( operation "name" -- )
   <builds , 
   does> ( a b -- d ) 
   @ ! 
   HWMULT_OP2 ! 
-  hwmult:16x16accum ;
-HWMULT_MPY hwmult:def_16bit_operation umac 
-HWMULT_MPYS hwmult:def_16bit_operation mac 
+  HWMULT_RESLO @ HWMULT_RESHI @ ;
 HWMULT_MAC hwmult:def_16bit_operation su*>d 
 HWMULT_MACS hwmult:def_16bit_operation s*>d 
+HWMULT_MPY hwmult:def_16bit_operation umac 
+HWMULT_MPYS hwmult:def_16bit_operation mac 
 
 : hwmult:def_32bit_operation_out64 ( opl oph "name" -- )
   <builds , , 
@@ -71,16 +50,17 @@ HWMULT_MACS hwmult:def_16bit_operation s*>d
   @ ! \ store the upper half into arg1
   cell+ ! \ store the lower half into arg2
   swap ( d0H d0L )
-  HWMULT_OP2L !
+  HWMULT_OP2L ! 
   HWMULT_OP2H !
-  hwmult:32x32accum ;
-HWMULT_MPY32L HWMULT_MPY32H hwmult:def_32bit_operation_out64 o32*o32->o64
-HWMULT_MPYS32L HWMULT_MPYS32H hwmult:def_32bit_operation_out64 i32*i32->i64
-HWMULT_MAC32L HWMULT_MAC32H hwmult:def_32bit_operation_out64 o32*o32+o64->o64
-HWMULT_MACS32L HWMULT_MACS32H hwmult:def_32bit_operation_out64 i32*i32+i64->i64
-: d* ( d0 d1 -- d ) i32*i32->i64 2drop ;
-: ud* ( d0 d1 -- d ) o32*o32->o64 2drop ;
-: d*>q ( d0 d1 -- q ) i32*i32->i64 ;
-: ud*>q ( d0 d1 -- q ) o32*o32->o64 ;
-: d*+ ( d0 d1 -- q ) i32*i32+i64->i64 2drop ;
-: ud*+ ( d0 d1 -- q ) o32*o32+o64->o64 2drop ;
+  HWMULT_RES0 @ 
+  HWMULT_RES1 @
+  HWMULT_RES2 @ 
+  HWMULT_RES3 @ ;
+HWMULT_MPY32L HWMULT_MPY32H hwmult:def_32bit_operation_out64 ud*>q
+HWMULT_MPYS32L HWMULT_MPYS32H hwmult:def_32bit_operation_out64 d*>q 
+HWMULT_MAC32L HWMULT_MAC32H hwmult:def_32bit_operation_out64 ud*+>q
+HWMULT_MACS32L HWMULT_MACS32H hwmult:def_32bit_operation_out64 d*+>q
+: d* ( d0 d1 -- d ) d*>q 2drop ;
+: ud* ( d0 d1 -- d ) ud*>q 2drop ;
+: d*+ ( d0 d1 -- d ) d*+>q 2drop ;
+: ud*+ ( d0 d1 -- d ) ud*+>q 2drop ;
